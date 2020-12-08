42ab62b0bb606eea4419bc1ec25d07a70bcc8dcb
Armando Alfaro <alfarowebdeveloper@gmail.com>
2020-12-08 15:36:57 -0800

    adding all files

---
+++ js/app.js
@@ -0,0 +1,45 @@
+function Exodus() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+function Leviticus() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("lev");
+    var btnText = document.getElementById("levMyBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+function showMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("show");
+    var btnText = document.getElementById("MoreMyBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
